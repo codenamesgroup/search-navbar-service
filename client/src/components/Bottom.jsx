7944f44c6aabae6316efcbe41893858e4bbd3760
j-ornelas <johnnyoh1988@gmail.com>
2018-03-04 15:29:20 -0800

    Adds icons to bottom left navbar section

--- client/src/components/Bottom.jsx
+++ client/src/components/Bottom.jsx
@@ -4,9 +4,18 @@ export default function Bottom() {
   return (
     <div className="bottom">
       <span className="bottom-left">
-        <span className="restaurants">restaurants</span>
-        <span className="nightlife">nightlife</span>
-        <span className="home-services">home services</span>
+        <span className="restaurants">
+          <img className="icon" src="https://i.imgur.com/BD9gZQZ.png" />
+          restaurants
+        </span>
+        <span className="nightlife">
+          <img className="icon" src="https://i.imgur.com/mXR9DiS.png" />
+          nightlife
+        </span>
+        <span className="home-services">
+          <img className="icon" src="https://i.imgur.com/WCdJVCf.png" />
+          home services
+        </span>
         <span className="write-a-review">write a review</span>
         <span className="events">events</span>
         <span className="talk">talk</span>
